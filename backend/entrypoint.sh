1679ab86db282d167c9f7d9114aed3c708e87b6a
Hasssan3xl <elhassanehussaine@gmail.com>
2026-05-24 09:43:07 +0100

    removed db health check for production db

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,21 +3,23 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "Waiting for database..."
-DB_HOST=${DB_HOST:-db}
-DB_PORT=${DB_PORT:-5432}
+if [ "$DJANGO_ENV" != "production" ]; then
+  echo "Waiting for database..."
+  DB_HOST=${DB_HOST:-db}
+  DB_PORT=${DB_PORT:-5432}
 
-if command -v nc >/dev/null 2>&1; then
-  while ! nc -z $DB_HOST $DB_PORT; do
-    echo "Database not ready, waiting..."
-    sleep 1
-  done
-else
-  echo "nc not found, sleeping for 5s..."
-  sleep 5
+  if command -v nc >/dev/null 2>&1; then
+    while ! nc -z $DB_HOST $DB_PORT; do
+      echo "Database not ready, waiting..."
+      sleep 1
+    done
+  else
+    echo "nc not found, sleeping for 5s..."
+    sleep 5
+  fi
+
+  echo "Database is ready!"
 fi
-
-echo "Database is ready!"
 
 # Run migrations
 echo "Running migrations..."
